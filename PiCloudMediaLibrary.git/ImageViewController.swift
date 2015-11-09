be3148d9ca5031db110025b53a46f7fb1293789c
Federico Bertoli <federico.bertoli@outlook.com>
2015-11-09 11:38:17 +0100

    Sistemata gestione del contatore per gli swipe e sistemato caricamento random delle immagini

--- PiCloudMediaLibrary.git/ImageViewController.swift
+++ PiCloudMediaLibrary.git/ImageViewController.swift
@@ -32,7 +32,6 @@ class ImageViewController: UIViewController, UIScrollViewDelegate {
             imagesIndex++; print("Right")
         default: break
         }
-        print(imagesIndex)
         imagesIndex = (imagesIndex < 0) ? (images.count-1): imagesIndex % images.count
         imageView.image = images[imagesIndex]
     
@@ -89,7 +88,6 @@ class ImageViewController: UIViewController, UIScrollViewDelegate {
         set {
             images.append(newValue)
             imageView.image = images[imagesIndex]
-            imagesIndex = images.count
             imageView.sizeToFit()
             scrollView?.contentSize = imageView.frame.size
             spinner?.stopAnimating()
@@ -108,13 +106,8 @@ class ImageViewController: UIViewController, UIScrollViewDelegate {
             dispatch_async(dispatch_get_main_queue()) {
                     if url == self.imageURL[URLCounter] {
                 if imageData != nil {
-                    //
-                    //self.images.append(UIImage(data: imageData!))
                     self.image = UIImage(data: imageData!)
-                    //self.images.append(self.image)
-                    //imagesArray.append(self.image)
-                    print(URLCounter)
-                    print ("caricata immagine da URL: \(url)")
+                    
                     print("images.count:\(self.images.count)")
                     } //else {
                         //self.imagesIndex.
