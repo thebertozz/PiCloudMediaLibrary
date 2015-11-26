8c132bb04c11adc23ed9612191fb46f55168afae
Federico Bertoli <federico.bertoli@outlook.com>
2015-11-26 20:43:14 +0100

    Versione 1.0

---
+++ PiCloudMediaLibrary.git/Globals.swift
@@ -0,0 +1,9 @@
+//
+//  Globals.swift
+//  PiCloudMediaLibrary.git
+//
+//  Created by Federico Bertoli on 18/11/15.
+//  Copyright © 2015 Federico Bertoli. All rights reserved.
+//
+
+import Foundation
